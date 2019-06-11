f8367a7d4396c8961f2117da3691dd63c90d3a94
PLQ <714303222@qq.com>
2019-06-11 17:53:41 +0800

    20190611

---
+++ DotNet基础加强/05练习_异常处理/06_判断两个对象是否是同一个对象/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_判断两个对象是否是同一个对象
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();
+            p1.name = "张三";
+            p1.age = 18;
+            p1.gender = "男";
+
+            Person p2 = new Person();
+            p2.name = "张三";
+            p2.age = 18;
+            p2.gender = "男";
+
+            Person p3 = p1;
+
+            //使用Object.ReferenceEquals()方法来比较两个对象是否是同一个对象（在堆内存上是同一个对象）
+            Console.WriteLine("使用Object.ReferenceEquals()方法来比较两个对象是否是同一个对象");
+            //返回true或false
+            Console.WriteLine(Object.ReferenceEquals(p1, p2));
+            Console.WriteLine(Object.ReferenceEquals(p1, p3));
+            Console.WriteLine();
+
+            Console.WriteLine("使用==来判断");
+            Console.WriteLine(p1==p2);
+            Console.WriteLine(p1==p3);
+            Console.WriteLine();
+
+            Console.WriteLine("使用Object.Equals()来判断");
+            Console.WriteLine(p1.Equals(p2));
+            Console.WriteLine(p1.Equals(p3));
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Person
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+        public string gender { get; set; }
+    }
+}

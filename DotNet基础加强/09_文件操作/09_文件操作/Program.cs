afdb3f4db4c7aa37b5d6fcad9f20773c624e8ca4
PLQ <714303222@qq.com>
2019-07-25 09:30:06 +0800

    2019-07-25

---
+++ DotNet基础加强/09_文件操作/09_文件操作/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_文件操作
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string str = File.ReadAllLines();
+        }
+    }
+}
